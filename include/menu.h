e686c5699d9f5909bc6158c23bdaaec343422af0
paltus <paltusoff@gmail.com>
2022-09-09 00:59:34 +0300

    +menu class

---
+++ include/menu.h
@@ -0,0 +1,65 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include "string.h"
+#include "types.h"
+#include "user_control.h"
+
+
+class Menu_t 
+{
+
+    public:
+        enum START_MENU
+        {
+            START_GAME,
+            OPTIONS,
+            EXIT
+        };
+
+        enum GAME_MENU
+        {
+            NEW_BATTLE,
+            CONTINUE,
+            BACK
+        };
+
+        enum State_t
+        {
+            STATE_NOTHING,
+            STATE_START_MENU,
+            STATE_GAME_MENU,
+        };
+        Menu_t():Cursor(0)
+        {}
+        virtual ~Menu_t()
+        {}
+
+        void Do(User_control_t::cmd_t cmd);
+
+        void Set_state(State_t state);
+        State_t Get_state() const
+        {
+            return State;
+        }
+        void Set_cursor(int cursor)
+        {
+            Cursor = cursor;
+        }
+        int Get_cursor() const
+        {
+            return Cursor;
+        }
+        int Get_cursor_max() const
+        {
+            return Cursor_max;
+        }
+        const char** Get_menu_str() const;
+    private:
+        const char** Menu_str_ptr;
+        int Cursor;
+        int Cursor_max;
+        State_t State;
+};
+
+#endif // MENU_H
---
+++ src/menu.cpp
@@ -0,0 +1,69 @@
+#include "menu.h"
+
+
+
+const char* Start_menu_str[] = 
+{
+    "Start game",
+    "Options",
+    "Exit"
+};
+
+const char* Game_menu_str[] = 
+{
+    "New battle",
+    "Contimue",
+    "Back"
+};
+
+void Menu_t::Do(User_control_t::cmd_t cmd)
+{
+
+    switch (cmd)
+    {
+    case (User_control_t::DOWN):
+        Cursor++;
+        break;
+    case (User_control_t::UP):
+        Cursor--;
+        break;
+    default:
+        break;
+    }
+
+
+    if(Cursor >= Cursor_max)
+        Cursor = 0;
+    else if(Cursor <= -1)
+        Cursor = Cursor_max - 1;
+
+}
+
+void Menu_t::Set_state(State_t state)
+{
+    State = state;
+    Cursor = 0;
+
+    switch (state)
+    {
+    case STATE_NOTHING:     
+        Cursor_max = 0;        
+        break;
+    case STATE_START_MENU:  
+        Cursor_max = sizeof(Start_menu_str) / sizeof(Start_menu_str[0]);
+        Menu_str_ptr = Start_menu_str;
+        break;
+    case STATE_GAME_MENU:   
+        Cursor_max = sizeof(Game_menu_str) / sizeof(Game_menu_str[0]);
+        Menu_str_ptr = Game_menu_str;
+        break;
+    default:
+        break;
+    }
+    
+}
+
+const char** Menu_t::Get_menu_str() const
+{
+    return Menu_str_ptr;
+}
